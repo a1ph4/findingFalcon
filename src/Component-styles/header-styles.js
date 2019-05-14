d574a0cac1d6ada8a5b83a9f1397714e5b74e252
Jayant <jayant@admybrand.com>
2019-05-14 19:39:50 +0530

    header and styled component

---
+++ src/Component-styles/header-styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HeaderDiv = styled.div`
+  background-color: #222;
+  height: 70px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  color: white;
+`;
+export default HeaderDiv;
---
+++ src/Components/header/header.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import HeaderDiv from "./../../Component-styles/header-styles.js";
+
+class Header extends Component {
+  render() {
+    return <HeaderDiv>Finding Falcon</HeaderDiv>;
+  }
+}
+export default Header;
